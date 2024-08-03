e13d4c9cf461c156b2018f09b6ae0608420e49cf
Julian Cardia <juliancardia87@gmail.com>
2024-08-03 16:24:55 -0300

    cards dinamicas

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 import './sass/main.scss'
+import Handlebars from "handlebars";
+
+const start = async () => {
+   
+   try {
+    const respuesta = await fetch('templates/card.hbs')
+
+    if( !respuesta.ok ) {
+        throw new Error('No se obtiene la card')
+    }
+
+    const plantilla = await respuesta.text ()
+
+    const template = Handlebars.compile(plantilla)
+
+    const respuestaBack = await fetch('http://localhost:8080/productos/')
+
+    if ( !respuestaBack.ok ) {
+        throw new Error('Error con los productos', respuestaBack.status)
+    }
+    const dataProductos = await respuestaBack.json()
+
+    const data = { productos: dataProductos }
+
+    const html = template(data)
+
+     console.log(html)
+
+     const contenedorCards = document.querySelector('#contenedor-cards')
+
+        contenedorCards.innerHTML = html
+   } catch (error) {
+    console.log('[start]:', error)
+   }
+
+}
+window.addEventListener('DOMContentLoaded',  start)
